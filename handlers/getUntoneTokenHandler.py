eff24d6e69daaf44abf1f6d4483ac88f9067ec01
Sakuru <74860621+SakuruWasTaken@users.noreply.github.com>
2023-03-03 23:29:59 -0800

    fix small oversight in untone authentication

--- handlers/getUntoneTokenHandler.py
+++ handlers/getUntoneTokenHandler.py
@@ -14,12 +14,12 @@ class handler(requestsManager.asyncRequestHandler):
 		try:
 			key = self.get_argument("k")
 
-			query = glob.db.fetch("SELECT token, user_id FROM untone_id_login_tokens WHERE client_key = %s ORDER BY id DESC LIMIT 1", [key])
+			query = glob.db.fetch("SELECT token_plaintext, user_id FROM untone_id_login_tokens WHERE client_key = %s ORDER BY id DESC LIMIT 1", [key])
 
 			if query:
 				# TODO: get username in the first query
-				self.write(f'key|{query["token"]}|{userUtils.getUsername(query["user_id"])}')
-				glob.db.execute("UPDATE untone_id_login_tokens SET client_key = '0', token = %s WHERE client_key = %s", [hashlib.md5(query["token"].encode('utf-8')).hexdigest(), key])
+				self.write(f'key|{query["token_plaintext"]}|{userUtils.getUsername(query["user_id"])}')
+				glob.db.execute("UPDATE untone_id_login_tokens SET client_key = '0', token_plaintext = '0' WHERE client_key = %s", [key])
 				return
 
 			self.write("0")
